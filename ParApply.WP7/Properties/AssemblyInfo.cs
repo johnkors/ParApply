541464344e684dbacde96f01d8b1a57366eec64a
John Korsnes <johnkors@gmail.com>
2012-02-23 19:15:54 +0100

    Marketplace says I cannot go from English to Norwegian. Not allowed to go fewer languages supported. How dare they!

--- ParApply.WP7/Properties/AssemblyInfo.cs
+++ ParApply.WP7/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("nb-NO")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
